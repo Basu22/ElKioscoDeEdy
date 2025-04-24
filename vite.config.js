07d413cd5b59c486d840237e7514d124711c8408
Baso22 <basilio.ossvald@gmail.com>
2025-04-24 18:11:11 -0300

    Busqueda con ABM de Producto y Modal incluido

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://baso22.github.io/github-pages/', // Cambia la base a la URL de tu repositorio
+  base: 'https://basu22.github.io/github-pages/', // Cambia la base a la URL de tu repositorio
   server: {
     host: '0.0.0.0',   // Escucha en todas las interfaces
     port: 5173,        // (opcional) Elegí un puerto si no querés el por defecto
